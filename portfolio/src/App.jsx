46dba8621161a9b7a217ccbaf3f4cd9e9429438d
Kapil Adhikari <kapiladhikari2060@gmail.com>
2025-03-03 13:47:48 +0545

    narbar completed

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+// Styles
+import "./global.css";
+
+// Components
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+// Styles
+import "./NavbarStyles.css";
+
+import { Disclosure, MenuButton } from "@headlessui/react";
+import { Menu as HeadMenu } from "@headlessui/react";
+import { X, Menu as LucideMenu } from "lucide-react";
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Disclosure as="nav" className="bg-gray-800">
+      <div className="NavContainer">
+        <div className="container"></div>
+        <div className="relative flex h-16 items-center justify-between">
+          <div className="flex shrink-0 items-center">
+            <div className="picture">
+              <div className="pic">
+                <img src="kapil.png" className="size-20 rounded-full"></img>
+              </div>
+              <div className="text1 animatedText">KAPIL ADHIKARI,</div>
+            </div>
+          </div>
+
+          {/* Desktop View */}
+          <HeadMenu as="div" className={`menu ${isOpen ? "open" : ""}`}>
+            <MenuButton className="btn">ABOUT</MenuButton>
+            <MenuButton className="btn">EXPERIENCE</MenuButton>
+            <MenuButton className="btn">SKILLS</MenuButton>
+            <MenuButton className="btn">EDUCATION</MenuButton>
+            <MenuButton className="btn">PROJECTS</MenuButton>
+          </HeadMenu>
+
+          {/* Mobile View */}
+          <button className="mobileMenuBtn" onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? <X size={28} /> : <LucideMenu size={28} />}
+          </button>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"></div>
+        </div>
+      </div>
+    </Disclosure>
+  );
+}
+
+export default Navbar;
