baab24516b9be047e982a0d87b01ee8795ae6c94
Kapil Adhikari <kapiladhikari2060@gmail.com>
2025-03-18 16:02:05 +0545

    navbar adjusted

--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 
+// Images
+import MyImage from "../../assets/kapil.png";
+
 // Styles
 import "./NavbarStyles.css";
 
 import { Disclosure, MenuButton } from "@headlessui/react";
 import { Menu as HeadMenu } from "@headlessui/react";
-import { X, Menu as LucideMenu } from "lucide-react";
+import { X, Menu as LucideMenu, ArrowDownFromLine } from "lucide-react";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,14 +16,12 @@ function Navbar() {
   return (
     <Disclosure as="nav" className="bg-gray-800">
       <div className="NavContainer">
-        <div className="container"></div>
         <div className="relative flex h-16 items-center justify-between">
           <div className="flex shrink-0 items-center">
             <div className="picture">
-              <div className="pic">
-                <img src="kapil.png" className="size-20 rounded-full"></img>
+              <div className="text1 animatedText">
+                <span>KAPIL_ADHIKARI,</span>
               </div>
-              <div className="text1 animatedText">KAPIL ADHIKARI,</div>
             </div>
           </div>
 
@@ -37,6 +38,9 @@ function Navbar() {
           <button className="mobileMenuBtn" onClick={() => setIsOpen(!isOpen)}>
             {isOpen ? <X size={28} /> : <LucideMenu size={28} />}
           </button>
+          <div className="last">
+            <img src={MyImage} id="pic" className="size-30 rounded-full"></img>
+          </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"></div>
         </div>
       </div>
